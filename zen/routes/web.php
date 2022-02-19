b5b81ce80bf0d6026b3a76ef708da368b8d6ab56
Tan Wei Kang <tanweikang02@gmail.com>
2022-02-19 13:13:53 +0800

    created PayPalController and payment routes

--- zen/routes/web.php
+++ zen/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PayPalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+
+// PayPal
+Route::get('/create-transaction', [PayPalController::class, 'createTransaction'])->name('createTransaction');
+Route::get('/process-transaction', [PayPalController::class, 'processTransaction'])->name('processTransaction');
+Route::get('/success-transaction', [PayPalController::class, 'successTransaction'])->name('successTransaction');
+Route::get('/cancel-transaction', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
 
 require __DIR__.'/auth.php';
