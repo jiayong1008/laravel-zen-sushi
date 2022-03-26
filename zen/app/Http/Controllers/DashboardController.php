7a1a372bc9886fde333b1598a7d9173c48282619
Tan Wei Kang <tanweikang02@gmail.com>
2022-03-26 14:25:09 +0800

    added some text components to dashboard

--- zen/app/Http/Controllers/DashboardController.php
+++ zen/app/Http/Controllers/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Transaction;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
@@ -17,8 +19,19 @@ class DashboardController extends Controller
             abort(403, 'This route is only meant for admin.');
 
         // calculate revenue
+        $generatedRevenue = Transaction::sum("final_amount");
+
+        // calculate cost and profit to be done later!
+
+        // calculate number of order being placed
+        $totalOrder = Transaction::count();
+
+        // calculate times of discount code being used
+        $discountCodeUsed = Transaction::where("discount_id", "!=", null)->count();
+
+        // calculate number of customer
+        $numCustomer = User::where("role", "customer")->count();
         
-        
-        return view('dashboard'); 
+        return view('dashboard', compact("generatedRevenue", "discountCodeUsed", "totalOrder", "numCustomer")); 
     }
 }
--- zen/resources/views/dashboard.blade.php
+++ zen/resources/views/dashboard.blade.php
@@ -23,10 +23,12 @@
             <h1>Dashboard</h1>
         </div>
     </div>
+
+    <!-- first row -->
     <div class="row mt-5 justify-content-center">
         <div class="col-md-3 col-6 my-4 mx-2 p-2 bg-white">
             <h5>Generated Revenue</h5>
-            <h2 class="my-4">RM 10826.59</h2>
+            <h2 class="my-4">RM {{ $generatedRevenue }}</h2>
             <p class="small text-muted">sales generated from the system</p>
         </div>
         <div class="col-md-3 col-6 my-4 mx-2 p-2 bg-white">
@@ -40,11 +42,32 @@
             <p class="small text-muted">difference of revenue and cost</p>
         </div>
     </div>
+
+    <!-- second row -->
     <div class="row mt-5 justify-content-center">
-        <div class="col-md-5 col-12 mb-4 mx-2 p-2 bg-white">
+        <div class="col-md-3 col-6 my-4 mx-2 p-2 bg-white">
+            <h5>Total Orders</h5>
+            <h2 class="my-4">{{ $totalOrder }}</h2>
+            <p class="small text-muted">number of orders being placed by now</p>
+        </div>
+        <div class="col-md-3 col-6 my-4 mx-2 p-2 bg-white">
+            <h5>Discount Code Usage</h5>
+            <h2 class="my-4">{{ $discountCodeUsed }} times</h2>
+            <p class="small text-muted">discount code usage analysis</p>
+        </div>
+        <div class="col-md-3 col-6 my-4 mx-2 p-2 bg-white">
+            <h5>Total Customers</h5>
+            <h2 class="my-4">{{ $numCustomer }}</h2>
+            <p class="small text-muted">customer base of the system</p>
+        </div>
+    </div>
+
+    <!-- third row - charts -->
+    <div class="row mt-5 justify-content-center">
+        <div class="col-md-5 col-12 my-4 mx-2 p-2 bg-white">
             <h5>Bar + line chart</h5>
         </div>
-        <div class="col-md-5 col-12 mb-4 mx-2 p-2 bg-white">
+        <div class="col-md-5 col-12 my-4 mx-2 p-2 bg-white">
             <h5>Pie chart</h5>
         </div>
     </div>
