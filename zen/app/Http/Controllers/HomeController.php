571d9b501cc8580aef23aea8c3650b2cfe2d9cab
Jia Yong <jiayong1008@gmail.com>
2022-03-31 10:35:57 +0800

    Minor Home Controller changes

--- zen/app/Http/Controllers/HomeController.php
+++ zen/app/Http/Controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 // Programmer Name: Ms. Lim Jia Yong, Project Manager
-// Description: Direct admin to dashboard page, and other users to home page
-// Edited on: 28 March 2022
+// Description: Direct admin to dashboard page, kitchen staff to order page,
+//              and other users to home page
+// Edited on: 30 March 2022
 
 namespace App\Http\Controllers;
 
@@ -18,6 +19,8 @@ class HomeController extends Controller
     public function index() {
         if (Auth::user() && auth()->user()->role == 'admin')
             return redirect()->route('dashboard');
+        else if (Auth::user() && auth()->user()->role == 'kitchenStaff')
+            return redirect()->route('kitchenOrder');
         return view('home');
     }
 }
